21cce86b5d2543e77608a8011ae954625eca7130
Joe Hsu <jhsu.x1@gmail.com>
2018-04-24 09:44:12 -0700

    get running tests

--- src/WithNamesake.spec.tsx
+++ src/WithNamesake.spec.tsx
@@ -1,31 +1,19 @@
-import { mount } from 'enzyme';
+import { mount,shallow } from 'enzyme';
 import * as React from 'react';
-import { MemoryRouter, withRouter } from 'react-router';
-import { Router as RouterPlain, NamesakeProvider, WithNamesake } from './index';
-
-const Router = withRouter(RouterPlain);
-
-// function provide(content: any) {
-//   // return mount(<div><MemoryRouter><Router>{content}</Router></MemoryRouter></div>);
-//   return mount(content);
-// }
-
-// enzyme doesn't understand new Context and Provider
+import { NamesakeConsumer, NamesakeProvider, Router, WithNamesake } from './index';
 
 describe('WithNamesake', () => {
   it('should pass down routes', (done) => {
-    const wrapper = mount(<div>
-      <NamesakeProvider value={{}}>
-      </NamesakeProvider>
-    </div>);
+    const routeConfig = {
+      'hello': '/hello',
+    };
+    const wrapper = mount(
+      <Router routes={routeConfig}>
+        <NamesakeConsumer>{(routes: { route(routeName: string): string }) => {
+          expect(routes.route('hello')).toEqual(routeConfig.hello);
+          done();
+        }}</NamesakeConsumer>
+      </Router>
+    );
   });
-
-      // <NamesakeProvider value={{ routes: {} }}>
-      //   <WithNamesake>{({ routes }: { routes: {} }) => {
-      //     console.log('hello');
-      //     expect(routes).toBeTruthy();
-      //     done();
-      //     return <div></div>;
-      //   }}</WithNamesake>
-      // </NamesakeProvider>
 });
--- src/WithNamesake.tsx
+++ src/WithNamesake.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { NamesakeConsumer } from './index';
+// import { Route } from 'react-router-dom';
 
 export interface IWithNamesakeProps {
   children(props: {}): React.ReactNode;
 }
 
-export class WithNamesake extends React.Component<IWithNamesakeProps, {}> {
+export default class WithNamesake extends React.Component<IWithNamesakeProps, {}> {
   public render() {
-    return <NamesakeConsumer>{(routes) => this.props.children({ routes })}</NamesakeConsumer>;
+    return <NamesakeConsumer>{(routes) => this.props.children(routes)}</NamesakeConsumer>;
   }
 }
+
+// export class WithNamesakeRoute extends React.Component<{}, {}> {
+//  public render() {
+//    return <Route>{(...routeProps) => (<NamesakeConsumer>{(routes) => this.props.children({ ...routeProps, routes })}</NamesakeConsumer>)}</Route>;
+//  }
+// }
