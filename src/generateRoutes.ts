d9607ec3d84c87600b25288d770636cb74afc59a
Joe Hsu <jhsu.x1@gmail.com>
2018-05-01 17:23:27 -0700

    use a single function

--- src/generateRoutes.ts
+++ src/generateRoutes.ts
@@ -2,24 +2,20 @@ export interface IRoutesConfig {
   [key: string]: string;
 }
 
-function reduce(obj, prefix=null, basePath=null) {
-  let result = {};
-  for (const key of Object.keys(obj)) {
+export default function generateRoutes(routes: {}, prefix?: string, basePath?: string): IRoutesConfig {
+  let result: IRoutesConfig = {};
+  for (const key of Object.keys(routes)) {
     const routeName = prefix ? `${prefix}.${key}` : key;
-    if (obj[key] instanceof Object) {
-      const path = prefix ? `${basePath}${obj[key].path}` : obj[key].path;
+    if (routes[key] instanceof Object) {
+      const path = prefix ? `${basePath}${routes[key].path}` : routes[key].path;
       result[routeName] = path;
 
-      const childRoutes = reduce(obj[key].children || {}, routeName, path);
+      const childRoutes = generateRoutes(routes[key].children || {}, routeName, path);
       result = {...result, ...childRoutes};
     } else {
-      const path = prefix ? `${basePath}${obj[key]}` : obj[key];
+      const path = prefix ? `${basePath}${routes[key]}` : routes[key];
       result[routeName] = path;
     }
   }
   return result;
-}
-
-export default function generateRoutes(routes: {}): IRoutesConfig {
-  return reduce(routes);
 }
