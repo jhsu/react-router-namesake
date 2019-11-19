ebacc879372f6a6ad3041ac3db2a69c3833e0296
Joe Hsu <jhsu.x1@gmail.com>
2019-11-18 20:08:01 -0800

    update example

--- examples/basic-nav/src/index.tsx
+++ examples/basic-nav/src/index.tsx
@@ -1,15 +1,34 @@
 import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { NamedRoutes, Link, Route } from "react-router-namesake";
+import {
+  useNamesake,
+  NamedRoutes,
+  Link,
+  Route,
+  Switch
+} from "react-router-namesake";
 
 import "./styles.css";
 
 const routes = {
   about: "/about",
   home: "/",
-  page: "/pages/:pageId"
+  pages: "/pages",
+  "page.show": "/pages/:page/show"
 };
+
+interface ButtonProps {
+  children: React.ReactNode;
+  to: string;
+  params?: {};
+}
+const Button: React.FC<ButtonProps> = ({ children, to, params = {} }) => {
+  const { transitionTo } = useNamesake();
+
+  return <button onClick={() => transitionTo(to, params)}>{children}</button>;
+};
+
 function App() {
   return (
     <BrowserRouter>
@@ -20,16 +39,29 @@ function App() {
           <Link to="home">home</Link> | <Link to="about">about</Link>
           <Route exact path="home">
             <div>
-              home path.
-              <Link to="page" params={{ pageId: 1 }}>Go to page 1</Link>
+              home path
+              <Button to="page.show" params={{ page: 123 }}>
+                see page 123
+              </Button>
             </div>
           </Route>
           <Route path="about">
-            <div>about path</div>
+            <div>
+              about path
+              <Button to="home">back home</Button>
+            </div>
           </Route>
-          <Route path="page">
-            <div>about path</div>
-          </Route>
+          <Switch>
+            <Route path="page.show">
+              <div>
+                show page
+                <Button to="home">back home</Button>
+              </div>
+            </Route>
+            <Route path="pages">
+              <div>pages index</div>
+            </Route>
+          </Switch>
         </div>
       </NamedRoutes>
     </BrowserRouter>
